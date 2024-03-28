5ac86272cf8f69f2a5db02a58e9169ca2424f78a
Bhargav Gadekar <87870903+Otakubhai@users.noreply.github.com>
2024-03-28 18:38:03 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+API_ID = 16768772  # Replace with your actual API ID
+API_HASH = "08d78fb05bdb90f1be4a4f1f0fef5f1e"  # Replace with your actual API hash
+BOT_TOKEN = "6780391059:AAETaBDjTNAPyQR1ed41WvzG1EV2F6GlszY"  # Replace with your actual bot token
+CHANNEL_USERNAME = "your_channel_username"  # Replace with your Telegram channel username
+MONGODB_URI = "mongodb+srv://luffyx7819:R0xYGEYRui9e2ScP@cluster1.lyqtfz7.mongodb.net/?retryWrites=true&w=majority"  # Replace with your MongoDB connection URI
+CHECK_INTERVAL = 300  # Check for new items every 5 minutes (in seconds)
